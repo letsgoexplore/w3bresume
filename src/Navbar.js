fa026d43eebe8eb53668add1f01de82f25047082
calif <751531219@qq.com>
2023-05-19 15:31:01 +0800

    merge

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,34 +1,30 @@
-import { Box, Flex, Image, Button, Text, Link, Spacer} from "@chakra-ui/react";
+import { Box, Flex, Image, Button, Text, Link, Spacer, Spacer} from "@chakra-ui/react";
 import logo from './logo.svg'; // 导入你的 logo 图片，需要将路径替换为你的 logo 图片路径
 import WalletButton from "./wallet";
 import TwitterButton from "./link_account/twitter"
 
-const Navbar = ({ isConnected, address }) => {
-    address = "0x1234567890123456789012345678901234567890";
-  return (
-    <Flex align="center" justify="space-between" py={4} px={10} bg="teal.500" color="white">
-      <Flex align="center">
-        <Link to="/App">
-            <Image src={logo} boxSize="50px" />
-        </Link>
-        <Spacer mx={3} />
-        <Link to="/App">
-            <Text>Experience</Text>
-        </Link>
-        <Spacer mx={3} />
-        <Text>|</Text>
-        <Spacer mx={3} />
-        <Link to="/job">  
-            <Text ml={3}>Job</Text>
-        </Link>
-      </Flex>
-      <Flex ml="auto">
-        <TwitterButton />
-        <WalletButton />
-      </Flex>
-    </Flex>
-  );
-};
-
-export default Navbar;
-
+export default function Navbar() {
+    const walletAddress = "0x1234567890123456789012345678901234567890";
+    const navigate = useNavigate();
+    return (
+        <Flex align="center" justify="space-between" py={4} px={10} bg="teal.500" color="white" style={{ fontFamily: 'Now bold' }}>
+            <Flex>
+                <button style={{ marginRight: '20px' }} onClick={() => navigate('/')}>
+                    <img src="./logo.svg" />                                        
+                </button>
+                <button style={{ marginRight: '20px' }} onClick={() => navigate('/')}>Experience</button>
+                <button ml={3} onClick={() => navigate('/job')}>Job</button>
+                <button onClick={() => navigate('https://api.twitter.com/oauth/authorize?oauth_token=7igv71Fz4GILEzmbrNF2exEa4')}>
+                    Authorize Twitter
+                </button>
+            </Flex>
+            <Spacer />
+            <Flex>
+                {/* <MetaMaskButton.Outline size="small">Connect with MetaMask</MetaMaskButton.Outline>
+                <Button colorScheme="teal">{walletAddress.slice(0,8)}</Button> */}
+                    <TwitterButton />
+                    <WalletButton />
+            </Flex>
+        </Flex>
+    );
+}
