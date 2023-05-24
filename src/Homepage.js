01433b1c79a86e84d234d4f09d5872ab8e6fd899
Jason Zhao <1531186050@qq.com>
2023-05-24 12:05:11 +0800

    modification

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,13 +1,15 @@
 import Navbar from "./navbar";
 import { Box, Flex, Heading } from "@chakra-ui/react";
-import HomepageImg from "./asset/homepage.png"
+import HomepageImg1 from "./asset/homepage1.png"
+import HomepageImg2 from "./asset/homepage2.png"
+import HomepageImg3 from "./asset/homepage3.png"
 
 export default function Homepage () {
     return (
         <Box bg="#fdefef">
           <Navbar />
     
-          <Flex align="center" justify="center" bgImage={`url(${HomepageImg})`} h="600px" bgSize="cover" bgPosition="center">
+          <Flex align="center" justify="center" bgImage={`url(${HomepageImg1})`} h="600px" bgSize="cover" bgPosition="center">
             {/* <Box textAlign="center" py={8} color={"red.500"}>
                 <Heading as="h1" size="3xl"><span style={{fontFamily:"29LT Baseet"}}> W3BResumes:</span> <span style={{color: "#0292b7"}}>Empowering</span></Heading>
                 <Heading as="h1" size="3xl">Your Professional Journey</Heading>
@@ -15,6 +17,8 @@ export default function Homepage () {
                 <Heading as="h2" size="lg" style={{fontFamily:'Century Gothic'}} color="red.600">Seize Opportunities with Web3 Resumes</Heading>
             </Box>  */}
           </Flex>
+          <Flex align="center" justify="center" bgImage={`url(${HomepageImg2})`} h="600px" bgSize="cover" bgPosition="center"></Flex>
+          <Flex align="center" justify="center" bgImage={`url(${HomepageImg3})`} h="600px" bgSize="cover" bgPosition="center"></Flex>
         </Box>
       );
 }
